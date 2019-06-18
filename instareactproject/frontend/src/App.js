a215d8cc8473a5a2bdf0ead4564b2e6529165496
Allen Thich <allenthich@gmail.com>
2019-06-18 16:24:38 -0700

    Create basic SearchComponent

--- instareactproject/frontend/src/App.js
+++ instareactproject/frontend/src/App.js
@@ -1,25 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class SearchComponent extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [],
+      query: "",
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Search</h3>
+        <MediaList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-search">
+            Enter a movie or TV-series:
+          </label>
+          <input
+            id="new-search"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>Search</button>
+        </form>
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({query: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+    // TODO: Search query via API and display results
+  }
+}
+
+class MediaList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SearchComponent/>
   );
 }
 
